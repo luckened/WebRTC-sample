abcc01a2d24c19facb2ab29c89b338061cbcc5ff
Leonardo Leite <42722914+LeonardoLeiteMeira@users.noreply.github.com>
2021-04-11 12:44:47 -0300

    finished layout from create room page

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CreateRoom from "./routes/CreateRoom";
 import Room from "./routes/Room";
+import Footer from './components/footer'
+import './index.css'
 
 const App = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={CreateRoom} />
-            <Route path="/room/:roomID" component={Room} />
-        </Switch>
-    </BrowserRouter>
+    <div className="app">
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={CreateRoom} />
+                <Route path="/room/:roomID" component={Room} />
+            </Switch>
+        </BrowserRouter>
+        <Footer />
+    </div>
 );
 
 export default App;
---
+++ client/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <p>Project developed by Davi Emediato, Gabriel Ribeiro, Leonardo Leite, Lucca Miranda, Rafael Silvério, Sávio  Cabral, Clara, Nicolas </p>
+        </div>
+    );
+}
+
+
+export default Footer;
