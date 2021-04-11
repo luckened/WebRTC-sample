281c49f6aecd4ef4982be6f04e83e009015e8074
Leonardo Leite <42722914+LeonardoLeiteMeira@users.noreply.github.com>
2021-04-11 12:44:47 -0300

    finished layout from create room page

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -1,5 +1,8 @@
 import React from "react";
+import DisneyMeetsLogo from "../images/DisneyMeets.png";
+import DisneyMeetsMainLogo from "../images/DisneyMeetsLogo.png";
 import { v1 as uuid } from "uuid";
+import "./createRoom.css";
 
 const CreateRoom = (props) => {
     const create = () => {
@@ -7,7 +10,34 @@ const CreateRoom = (props) => {
         props.history.push(`/room/${id}`);
     };
 
-    return <button onClick={create}>Create Room</button>;
+    return (
+        <div className="page">
+            <div className="mainContent itemGrid">
+                <header className="header">
+                    <img className="headerLogo" src={DisneyMeetsMainLogo} />
+                    <p>Disney Meets</p>
+                </header>
+                <section className="titleSection">
+                    <h1 className="title">WELCOME TO DISNEY MEETS</h1>
+                    <p>The best application to online meet</p>
+                </section>
+
+                <section className="buttonArea">
+                    <p>
+                        To create a room click on the button and send the link
+                        to a friend
+                    </p>
+                    <button className="button" onClick={create}>
+                        Create Room
+                    </button>
+                </section>
+            </div>
+
+            <section className="logoSide itemGrid">
+                <img className="disneyLogo" src={DisneyMeetsLogo} />
+            </section>
+        </div>
+    );
 };
 
 export default CreateRoom;
